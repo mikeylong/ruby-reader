5cff255377018396d0c692dd4c15172e5fe64fb4
Mike Long <sequence3@gmail.com>
2012-05-24 16:30:20 -0500

    find element with most text content.

--- get_primary_content.rb
+++ get_primary_content.rb
@@ -2,9 +2,33 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML( open('http://mblongii.com') )
-doc.at_css('body').traverse do |node|
-  if node.element?
-    p node.content
+# url = 'http://mblongii.com'
+# url = 'html/long_article.html'
+# url = 'html/all_articles.html'
+url = 'html/short_article.html'
+
+ELEMENTS = []
+elements = File.open('ELEMENTS').read
+elements.each_line {|line|
+  ELEMENTS.push line.strip
+}
+
+# Find out which element has the most content and if that element is in the 'elements' list
+def find_element_with_most_content(url)
+  doc = Nokogiri::HTML( open(url) )
+  nodes = []
+  doc.at_css('body').traverse do |node|
+    if node.element?
+      for tag in ELEMENTS
+        nodes.push node.name,node.children.count,node.text
+      end
+    end
   end
+  p nodes
 end
+
+def get_primary_content(url)
+  find_element_with_most_content(url)
+end
+
+get_primary_content(url)
