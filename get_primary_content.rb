aa348081ad9ef21932ca47f6a4431f739d1ff76b
Mike Long <sequence3@gmail.com>
2012-05-29 07:25:03 -0700

    count content length of each element

--- get_primary_content.rb
+++ get_primary_content.rb
@@ -4,8 +4,8 @@ require 'open-uri'
 
 # url = 'http://mblongii.com'
 # url = 'html/long_article.html'
-# url = 'html/all_articles.html'
-url = 'html/short_article.html'
+url = 'html/all_articles.html'
+# url = 'html/short_article.html'
 
 ELEMENTS = []
 elements = File.open('ELEMENTS').read
@@ -16,15 +16,15 @@ elements.each_line {|line|
 # Find out which element has the most content and if that element is in the 'elements' list
 def find_element_with_most_content(url)
   doc = Nokogiri::HTML( open(url) )
-  nodes = []
-  doc.at_css('body').traverse do |node|
-    if node.element?
-      for tag in ELEMENTS
-        nodes.push node.name,node.children.count,node.text
+  nodes = Hash.new(0)
+  for tag in ELEMENTS
+    doc.at_css(tag).traverse do |node|
+      if node.element?
+        nodes = {"name" => node.name, "length" => node.content.length, "content" => node.content}
       end
     end
+    p nodes['length']
   end
-  p nodes
 end
 
 def get_primary_content(url)
